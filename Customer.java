c72205ada290d772a089a66f68da4cd72cc43e28
KelvinHuang2701 <115201372+KelvinHuang2701@users.noreply.github.com>
2023-03-22 16:16:18 -0400

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,12 +1,30 @@
 public class Customer {
     private int customerID;
+    private Account savingsAccount;
+    private Account checkingAccount;
+    private Account creditAccount;
 
     public Customer(int customerID) {
         this.customerID = customerID;
+        this.savingsAccount = new Account();
+        this.checkingAccount = new Account();
+        this.creditAccount = new Account();
     }
 
     public int getCustomerID() {
         return customerID;
+    }
+    
+    public Account getSavingsAccount() {
+        return savingsAccount;
+    }
+    
+    public Account getCheckingAccount() {
+        return checkingAccount;
+    }
+    
+    public Account getCreditAccount() {
+        return creditAccount;
     }
 
     public boolean login(Card card, int pin) {
