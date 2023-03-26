406da891c0510e3a9fdefff0b075d5e648001d43
Sadaf Saadman Jawad <96027974+sadafjawad@users.noreply.github.com>
2023-03-26 16:06:46 -0400

    Update Transaction.java

--- Transaction.java
+++ Transaction.java
@@ -4,7 +4,6 @@ public class Transaction {
     private Customer customer;
     private Account account;
 
-    // constructor method
     public Transaction(String date, Double amount, Customer customer, Account account) {
         this.date = date;
         this.amount = amount;
@@ -12,47 +11,43 @@ public class Transaction {
         this.account = account;
     }
 
-    // printing the receipt of an account transaction
     public String getReceipt(Transaction t) {
-        String output = "";
-        output += "\n";
-        output += "-----------------------------\n";
-        output += "Date: " + t.getDate() + "\n";
-        output += "Account: " + t.getAccount().getAccountNumber() + "\n";
-        output += "Amount: " + amount + "\n";
-        output += "-----------------------------\n";
+        String output = "<html>";
+        output += "<br>";
+        output += "-----------------------------<br>";
+        output += "Date: " + t.getDate() + "<br>";
+        output += "Account: " + t.getAccount().getAccountNumber() + "<br>";
+        output += "Amount: " + amount + "<br>";
+        output += "-----------------------------<br>";
+        output += "<html>";
         return output;
     }
 
-    // printing the receipt of an transfer transaction between two accounts
     public String getReceipt(Transaction t1, Transaction t2) {
-        String output = "";
-        output += "\n";
-        output += "-----------------------------\n";
-        output += "Date: " + date + "\n";
-        output += "Transfer From Account: " + t1.getAccount().getAccountNumber() + "\n";
-        output += "Transfer To Account: " + t2.getAccount().getAccountNumber() + "\n";
-        output += "Amount: " + amount + "\n";
-        output += "-----------------------------\n";
+        String output = "<html>";
+        output += "<br>";
+        output += "-----------------------------<br>";
+        output += "Date: " + date + "<br>";
+        output += "Transfer From Account: " + t1.getAccount().getAccountNumber() + "<br>";
+        output += "Transfer To Account: " + t2.getAccount().getAccountNumber() + "<br>";
+        output += "Amount: " + amount + "<br>";
+        output += "-----------------------------<br>";
+        output += "<html>";
         return output;
     }
 
-    // method to get transaction date
     public String getDate() {
         return date;
     }
 
-    // method to get the transaction amount
     public Double getAmount() {
         return amount;
     }
 
-    // method to get the customer of the transaction
     public Customer getCustomer() {
         return customer;
     }
 
-    // method to get the account involved in the transaction
     public Account getAccount() {
         return account;
     }
